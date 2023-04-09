c3a1ef3d315daa2d6dcc6b390fdeae615489c648
Ryan Xu <ryan.xu@Ryans-Air.lan>
2023-04-08 19:01:43 -0700

    refacted battle and cleaned up code

--- battle.pl
+++ battle.pl
@@ -1,51 +1,40 @@
 :- [pokemon, damage].
 
-% create a loop of battle
+% Create a loop of battle
 battle(Player, PlayerHP, Computer, ComputerHP) :-
     PlayerHP>0, ComputerHP>0,
     promptMessages(Player, PlayerHP, Computer, ComputerHP),
-    pokemon(Player, _, _, _, _, _, PlayerMoves), writeln(PlayerMoves),
 
-    read(PlayerMove),
-    calculate_damage(Player, Computer, PlayerMove, PlayerDamage),
-    NewComputerHP is ComputerHP-PlayerDamage,
+    playersTurn(Player, Computer, ComputerHP, NewComputerHP),
+    (NewComputerHP =< 0 -> writeln('\nVictory!'), !;
 
-    format('~n~w used ~w! ~w lost ~w health.~n', [Player, PlayerMove, Computer, PlayerDamage]),
-    format('~w has ~w health left.~n~n', [Computer, NewComputerHP]),
-
-    (NewComputerHP =< 0 -> writeln('Victory!'), !;
-
-    computer_choose_move(Computer, ComputerMove),
-    calculate_damage(Computer, Player, ComputerMove, ComputerDamage),
-    NewPlayerHP is PlayerHP-ComputerDamage,
-
-    writeln("Computer's turn:"),
-
-    format('~w used ~w! ~w lost ~w health.~n', [Computer, ComputerMove, Player, ComputerDamage]),
-    format('~w has ~w health left.~n', [Player, NewPlayerHP]),
-
-    (NewPlayerHP =< 0 -> writeln('Defeat!'), !;
+    computersTurn(Computer, Player, PlayerHP, NewPlayerHP),
+    (NewPlayerHP =< 0 -> writeln('\nDefeat!'), !;
 
     battle(Player, NewPlayerHP, Computer, NewComputerHP))).
+
+promptMessages(Player, PlayerHP, Computer, ComputerHP) :-
+    write('\nPlayer: '), write(Player), write(' | Computer: '), write(Computer),
+    write('\nPlayer HP: '), write(PlayerHP), write(' | Computer HP: '), writeln(ComputerHP).
+
+attack(Attacker, Defender, AttackerMove, DefenderOriginalHP, DefenderRemainingHP) :-
+    calculate_damage(Attacker, Defender, AttackerMove, AttackerDamage),
+    DefenderRemainingHP is DefenderOriginalHP-AttackerDamage,
+    format('~w used ~w! ~w lost ~w health.~n', [Attacker, AttackerMove, Defender, AttackerDamage]),
+    format('~w has ~w health left.~n', [Defender, DefenderRemainingHP]).
+
+playersTurn(Player, Computer, ComputerHP, NewComputerHP) :-
+    pokemon(Player, _, _, _, _, _, PlayerMoves),
+    write('\nChoose your move:'),
+    writeln(PlayerMoves), read(PlayerMove),
+    writeln("\nPlayer's turn:"),
+    attack(Player, Computer, PlayerMove, ComputerHP, NewComputerHP).
+
+computersTurn(Computer, Player, PlayerHP, NewPlayerHP) :-
+    computer_choose_move(Computer, ComputerMove),
+    writeln("\nComputer's turn:"),
+    attack(Computer, Player, ComputerMove, PlayerHP, NewPlayerHP).
 
 computer_choose_move(Computer, ChosenMove) :-
     pokemon(Computer, _, _, _, _, _, Moves),
     random_member(ChosenMove, Moves).
-    % write('Computer picked:'), writeln(ChosenMove).
-
-promptMessages(Player, PlayerHealth, Computer, ComputerHealth) :-
-    writeln(''),
-    write('Player: '),writeln(Player), write('Player HP: '),writeln(PlayerHealth),
-    writeln(''),
-    write('Computer: '),writeln(Computer), write('Computer HP: '),writeln(ComputerHealth),
-    writeln(''),
-    write('Choose your move:').
-    
-
-% turn of computer, randomly choose the move
-% c_turn_randomly_choose_move(Computer,Player,Damage,Health) :-
-%     pokemon(Computer,_,_,_,_,_,Moves),
-%     random_member(Move,Moves),
-%     % damage(Computer,Player,Move,Damage).
-%     calculate_health(Player, Move, NewHealth),
-%     Health is NewHealth.
--- damage.pl
+++ damage.pl
@@ -1,53 +1,16 @@
 :- [pokemon, move, weakness].
 
-% damage(Attacker, Defender, Move, Damage) :-
-%     pokemon(Attacker, Type_1, HP_1, Attack_1, Defense_1, Speed_1, Moves_1),
-%     member(Move, Moves_1),
-%     move(_,_,BaseDamage),
-%     BaseDamage > 0,
-%     pokemon(Defender, Type_2, HP_2, Attack_2, Defense_2, Speed_2, Moves_2),
-%     % (Attacker=Defender;dif(Attacker,Defender)),
-%     % (Type_1=Type_2;dif(Type_1,Type_2)),
-%     % (HP_1=HP_2;dif(HP_1,HP_2)),
-%     % (Attack_1=Attack_2;dif(Attack_1,Attack_2)),
-%     % (Defense_1=Defense_2;dif(Defense_1,Defense_2)),
-%     % (Speed_1=Speed_2;dif(Speed_1,Speed_2)),
-%     % (Moves_1=Moves_2;dif(Moves_1,Moves_2)),
-%     move(Move, MoveType, BaseDamage),
-%     effectFactor(MoveType,Type_2, Multiplier),
-%     Damage is (BaseDamage*Attack_1)*Multiplier/Defense_2.
-
-% redesigned damage function
-% damage(Attacker, Defender, Move, Damage) :-
-%     pokemon(Attacker, attack, A),
-%     pokemon(Attacker, move, M),
-%     member(Move, M),
-%     move(Move, MoveType, BaseDamage),
-%     BaseDamage > 0,
-%     pokemon(Defender, defense, D),
-%     pokemon(Defender, type, T),
-%     effectFactor(MoveType, T, Multiplier),
-%     Damage is ((BaseDamage * A) * Multiplier)/D.
-
 calculate_damage(Attacker, Defender, Move, Damage) :-
-
-    % attacker (only need its: type, attack)
     pokemon(Attacker, _, _, AttackerAttack, _, _, _),
-    % defender (only need its: type, hp, defense)
     pokemon(Defender, DefenderType, _, _, DefenderDefense, _, _),
-
-    % get move and its type and damage
     move(Move, MoveType, MoveDamage),
-
     calculate_adjusted(AttackerAttack, MoveType, MoveDamage, DefenderType, DefenderDefense, Adjusted),
-    
-    % update adjusted damage
     Damage is Adjusted.
     
 calculate_adjusted(AttackerAttack, MoveType, MoveDamage, DefenderType, DefenderDefense, Adjusted) :-
-    effectiveness(MoveType, DefenderType, Modifier),
+    calculate_modifier(MoveType, DefenderType, Modifier),
     Adjusted is Modifier * (0.5 + 0.5 * AttackerAttack / DefenderDefense) * MoveDamage.
     
-effectiveness(MoveType, DefenderType, Modifier) :-
+calculate_modifier(MoveType, DefenderType, Modifier) :-
     (weakness(MoveType, DefenderType) -> 
         (resistance(MoveType, DefenderType) -> Modifier=1; Modifier=2); Modifier=1).
